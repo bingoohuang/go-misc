f60462bfde10af68b87a21eb4c745099c3f63439
bingoo <bingoo.huang@gmail.com>
2018-08-29 14:27:13 +0800

    right duplicate remove and servpack merge

---
+++ right_duplicate.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"os"
+	"strings"
+)
+
+type Right struct {
+	Id              string `db:"ID"`
+	RightCode       string `db:"RIGHT_CODE"`
+	RightName       string `db:"RIGHT_NAME"`
+	RightType       string `db:"RIGHT_TYPE"`
+	RightDesc       string `db:"RIGHT_DESC"`
+	ServpackId      string `db:"SERVPACK_ID"`
+	Url             string `db:"URL"`
+	ChainName       string `db:"CHAIN_NAME"`
+	ChainDefinition string `db:"CHAIN_DEFINITION"`
+	Order           string `db:"ORDER"`
+}
+
+func init()  {
+	dsn = os.Args[1] // "user:pass@tcp(ip:3306)/db?charset=utf8"
+}
+
+var dsn  string
+
+func removeDuplicate() {
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("Disabling MySQL tests:\n    %v", err)
+		return
+	}
+	defer db.Close()
+
+	rights := []Right{}
+
+	err = db.Select(&rights, "select ID, RIGHT_CODE, RIGHT_NAME, RIGHT_TYPE, RIGHT_DESC, COALESCE(SERVPACK_ID,'') SERVPACK_ID, "+
+		"COALESCE(`URL`, '') URL, COALESCE(CHAIN_NAME, '') CHAIN_NAME, COALESCE(CHAIN_DEFINITION, '') CHAIN_DEFINITION, `ORDER` "+
+		"from tt_d_funcright order by right_code, servpack_id")
+	if err != nil {
+		fmt.Printf("Select:\n    %v", err)
+		return
+	}
+
+	duplicates := 0
+	duplicateIds := []string{}
+	for i := 0; i+1 < len(rights); {
+		l := rights[i]
+		r := rights[i+1]
+
+		lid := l.Id
+		rid := r.Id
+
+		l.Id = ""
+		r.Id = ""
+
+		if l == r {
+			fmt.Printf("Found Duplicats: %v\n", lid)
+		}
+
+		i++
+
+		for ; l == r; i++ {
+			duplicates++
+			r.Id = rid
+			duplicateIds = append(duplicateIds, r.Id)
+			fmt.Printf("Duplicate:%v\n", r)
+			r = rights[1+i]
+			rid = r.Id
+			r.Id = ""
+		}
+
+		l.Id = lid
+		r.Id = rid
+	}
+
+	fmt.Printf("Duplicates:%v\n", duplicates)
+
+	if duplicates > 0 {
+		db.MustExec(`delete from tt_d_funcright where id in (` + strings.Join(duplicateIds, ",") + `)`)
+	}
+}
---
+++ right_merge.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"strings"
+)
+
+func mergeRights() {
+	db, err := sqlx.Connect("mysql", dsn)
+	if err != nil {
+		fmt.Printf("Disabling MySQL tests:\n    %v", err)
+		return
+	}
+	defer db.Close()
+
+	rights := []Right{}
+
+	err = db.Select(&rights, "select ID, RIGHT_CODE, RIGHT_NAME, RIGHT_TYPE, RIGHT_DESC, COALESCE(SERVPACK_ID,'') SERVPACK_ID, "+
+		"COALESCE(`URL`, '') URL, COALESCE(CHAIN_NAME, '') CHAIN_NAME, COALESCE(CHAIN_DEFINITION, '') CHAIN_DEFINITION, `ORDER` "+
+		"from tt_d_funcright order by right_code, servpack_id")
+	if err != nil {
+		fmt.Printf("Select:\n    %v", err)
+		return
+	}
+
+	merges := 0
+	duplicateIds := []string{}
+	for i := 0; i+1 < len(rights); {
+		l := rights[i]
+		r := rights[i+1]
+
+		lid := l.Id
+		rid := r.Id
+		l.Id = ""
+		r.Id = ""
+
+		lsp := l.ServpackId
+		lspMerged := lsp
+		rsp := r.ServpackId
+
+		l.ServpackId = ""
+		r.ServpackId = ""
+
+		hasMerged := false
+		if l == r {
+			fmt.Printf("Found Mergables: %v\n", lid)
+			hasMerged = true
+		}
+
+		i++
+
+		for ; l == r; i++ {
+			lspMerged += "," + rsp
+			merges++
+			duplicateIds = append(duplicateIds, rid)
+			r.Id = rid
+			r.ServpackId = rsp
+			fmt.Printf("Mergeable:%v\n", r)
+			r = rights[1+i]
+			rid = r.Id
+			r.Id = ""
+
+			rsp = r.ServpackId
+			r.ServpackId = ""
+		}
+
+		l.Id = lid
+		r.Id = rid
+
+		l.ServpackId = lsp
+		r.ServpackId = rsp
+
+		if hasMerged {
+			db.MustExec("update tt_d_funcright set SERVPACK_ID = ? where id = ?", lspMerged, lid)
+		}
+	}
+
+	fmt.Printf("merges:%v\n", merges)
+
+	if merges > 0 {
+		db.MustExec(`delete from tt_d_funcright where id in (` + strings.Join(duplicateIds, ",") + `)`)
+	}
+}
